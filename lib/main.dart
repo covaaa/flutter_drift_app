dff18bb347b63786c1f48ef5525de1ef799211c8
cova <cova8458@gmail.com>
2024-12-31 02:40:18 +0900

    add bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'dart:async';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_drift_app/src/core/core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: App()));
+  FlutterError.onError = (details) {
+    return log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+  return runZonedGuarded<void>(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      return runApp(const ProviderScope(child: App()));
+    },
+    (error, stackTrace) => log('$error', stackTrace: stackTrace),
+  );
 }
