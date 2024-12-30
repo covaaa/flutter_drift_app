e8c43263e69e849ba5621a99439fe68b030bf2b6
cova <cova8458@gmail.com>
2024-12-31 04:34:48 +0900

    move color seed to domain

--- lib/src/core/core.dart
+++ lib/src/core/core.dart
@@ -1,4 +1,4 @@
+export 'package:flutter_drift_app/src/core/domain/color_seed.dart';
 export 'package:flutter_drift_app/src/core/state/observers.dart';
 export 'package:flutter_drift_app/src/core/ui/app.dart';
-export 'package:flutter_drift_app/src/core/ui/color_seed.dart';
 export 'package:flutter_drift_app/src/core/ui/loading.dart';
