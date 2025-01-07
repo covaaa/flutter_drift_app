8dd985b0465f27c5485b41dd04d0b5ebf2a6151a
cova <cova8458@gmail.com>
2025-01-07 20:53:24 +0900

    fix: remove async run zoned guarded for checking ci

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:flutter_drift_app/src/features/account/account.dart';
 import 'package:flutter_drift_app/src/shared/core/core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
@@ -11,15 +10,18 @@ void main() {
     return log(details.exceptionAsString(), stackTrace: details.stack);
   };
   return runZonedGuarded<void>(
-    () async {
+    () {
       WidgetsFlutterBinding.ensureInitialized();
-      final container = ProviderContainer();
-      final preference = await TaskEither.tryCatch(
-        () async => container.read(readPreferenceProvider.future),
-        (error, stackTrace) => Failure('$error'),
-      ).run();
+      // final container = ProviderContainer();
+      // final preference = await TaskEither.tryCatch(
+      //   () async => container.read(readPreferenceProvider.future),
+      //   (error, stackTrace) => Failure('$error'),
+      // ).run();
       return runApp(
-        ProviderScope(observers: [RiverpodObserver()], child: App(preference)),
+        ProviderScope(
+          observers: [RiverpodObserver()],
+          child: const App(Left(Failure('Fake Exception'))),
+        ),
       );
     },
     (error, stackTrace) => log('$error', stackTrace: stackTrace),
